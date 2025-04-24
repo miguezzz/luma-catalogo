209e1edcff492b96944bcb24f1ba984b67381b87
Victor Miguez <v.miguez2004@gmail.com>
2025-04-24 13:52:05 -0300

    404 para categorias nao existentes

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,18 +1,43 @@
+import { notFound } from 'next/navigation';
+
+interface ICategoryItem {
+  id: number;
+  name: string;
+  slug: string;
+  description: string;
+  image: {
+    data: {
+      id: number;
+      attributes: {
+        url: string;
+      };
+    };
+  };
+};
+
 async function getCategoryItems(category: string) {
   const data = await fetch(`http://localhost:1337/api/products?filters[category][slug][$eq]=${category}&populate=*&pagination[pageSize]=50`);
+  const categoryItems = await data.json();
 
-  const categories = await data.json();
+  console.log(categoryItems);
 
-  return categories.data;
+  if (!categoryItems || categoryItems.data.length === 0) return null;
+
+  return categoryItems.data;
 }
 
 export default async function CategoryPage({ params }: { params: { category: string } }) {
-  const categoryItems = await getCategoryItems(params.category);
+  const categoryItemsFromAPI = await getCategoryItems(params.category);
+  if (!categoryItemsFromAPI) {
+    notFound();
+  }
 
   return (
-    <div>
-      Página de produtos da categoria {params.category}
-    </div>
-    // renderizar os produtos da categoria
+    <>
+      <div>
+        Página de produtos da categoria {params.category}
+      </div>
+
+    </>
   );
 }
