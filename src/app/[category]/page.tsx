94ae13df0e8e623d2adb10f5b6a79e053b44e9b1
Victor Miguez <v.miguez2004@gmail.com>
2025-04-26 13:25:34 -0300

    ProductItem Interface

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,18 +1,31 @@
 import { notFound } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+import Button from '@/components/Button';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
-interface ICategoryItem {
-  id: number;
-  name: string;
-  slug: string;
-  description: string;
-  image: {
-    data: {
-      id: number;
-      attributes: {
-        url: string;
-      };
-    };
-  };
+
+interface IProductItem {
+  id: number,
+  name: string,
+  slug: string,
+  description: string,
+  image: { url: string },
 };
 
 async function getCategoryItems(category: string) {
@@ -33,11 +46,42 @@ export default async function CategoryPage({ params }: { params: { category: str
   }
 
   return (
-    <>
+    <div className="flex flex-col justify-center items-center">
       <div>
         Página de produtos da categoria {params.category}
       </div>
 
-    </>
+      {/* product cards */}
+      <div className="grid grid-cols-5 gap-4 p-16 overflow-hidden">
+        {categoryItemsFromAPI.map((productItem: IProductItem) => (
+          <Card key={productItem.slug} className="flex bg-white/20 backdrop-blur-md rounded-xl shadow-xl max-w-sm border-none transform hover:shadow-[0_0_20px_rgba(255,255,255,1)] transition-all ease-in-out duration-300 h-140 overflow-hidden">
+            <CardHeader>
+              <CardTitle className="text-2xl">
+                {productItem.name}
+              </CardTitle>
+              <CardDescription className="text-xm text-shadow-sm text-gray-500 mb-2">
+                <p>{productItem.description}</p>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Image
+                src={
+                  productItem.image?.url ?
+                    `http://localhost:1337${productItem.image?.url}` :
+                    `https://picsum.photos/901`
+                }
+                alt={`Categoria ${productItem.name}`}
+                width={400}
+                height={400}
+                className="flex rounded-2xl object-cover object-center w-full h-85 mb-2"
+              />
+            </CardContent>
+            <CardFooter className="flex justify-center items-center rounded-3xl ">
+              <Button>Comprar</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div >
   );
 }
