ef42edf1237897a16cb337db5b9a5eba432316aa
Victor Miguez <v.miguez2004@gmail.com>
2025-04-29 16:23:13 -0300

    allow reqs from railway domain

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost", "picsum.photos"],
+    domains: ["localhost", "picsum.photos", "luma-catalogo-strapi-production.up.railway.app"],
   },
 };
 
