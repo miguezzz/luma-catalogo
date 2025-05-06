fe8854ddeaa7c4316bdfc7004871ffafb0b95ace
Victor Miguez <v.miguez2004@gmail.com>
2025-05-06 20:52:01 -0300

    fix: productcard importing modal dynamically

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import ProductModal from './ProductModal';
+// import ProductModal from './ProductModal';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import Button from './Button';
+import dynamic from 'next/dynamic';
+
+// Importa o modal apenas no client, sem SSR
+const ProductModal = dynamic(() => import('./ProductModal'), {
+  ssr: false,
+});
 
 interface Props {
   product: {
@@ -24,36 +30,58 @@ interface Props {
 }
 
 export default function ProductCard({ product }: Props) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   return (
-    <Card key={product.slug} className="flex bg-white/20 backdrop-blur-md rounded-xl shadow-xl max-w-sm border-none transform hover:shadow-[0_0_20px_rgba(255,255,255,1)] transition-all ease-in-out duration-300 h-140 overflow-hidden">
-      <CardHeader>
-        <CardTitle className="text-2xl">{product.name}</CardTitle>
-        <CardDescription className="text-xm text-shadow-sm text-gray-500 mb-2">
-          {product.description || ''}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Image
-          src={
-            product.images?.[0]?.url
-              ? `https://luma-catalogo-strapi-production.up.railway.app${product.images[0].url}`
-              : `https://picsum.photos/400`
-          }
-          alt={`Produto ${product.name}`}
-          width={400}
-          height={400}
-          className="flex rounded-2xl object-cover object-center w-full h-85 mb-2"
-        />
-      </CardContent>
-      <CardFooter className="flex justify-center s-center rounded-3xl">
-        <Button className="bg-white text-amber-700 hover:bg-amber-700 hover:text-white border-none font-bold py-2 px-8 rounded-3xl transform hover:shadow-[0_0_30px_#9b5300] transition-all ease-in-out duration-300">
+    <>
+      <Card key={product.slug} className="flex bg-white/20 backdrop-blur-md rounded-xl shadow-xl max-w-sm border-none transform hover:shadow-[0_0_20px_rgba(255,255,255,1)] transition-all ease-in-out duration-300 h-140 overflow-hidden">
+        <CardHeader>
+          <CardTitle className="text-2xl">{product.name}</CardTitle>
+          <CardDescription className="text-xm text-shadow-sm text-gray-500 mb-2">
+            {product.description || ''}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Image
+            src={
+              product.images?.[0]?.url
+                ? `https://luma-catalogo-strapi-production.up.railway.app${product.images[0].url}`
+                : `https://picsum.photos/400`
+            }
+            alt={`Produto ${product.name}`}
+            width={400}
+            height={400}
+            className="flex rounded-2xl object-cover object-center w-full h-85 mb-2"
+          />
+        </CardContent>
+        <CardFooter className="flex justify-center s-center rounded-3xl">
+          <Button onClick={() => setOpen(true)} className="bg-white text-amber-700 hover:bg-amber-700 hover:text-white border-none font-bold py-2 px-8 rounded-3xl transform hover:shadow-[0_0_30px_#9b5300] transition-all ease-in-out duration-300">
+            Comprar
+          </Button>
+        </CardFooter>
+      </Card>
 
-          Comprar
-        </Button>
-      </CardFooter>
-    </Card>
-  )
+      {open && (
+        <ProductModal
+          product={{
+            id: product.id,
+            name: product.name,
+            description: product.description,
+            SKU: product.SKU,
+            inStock: product.inStock,
+            slug: product.slug,
+            qty: 0, // Default value, update as needed
+            imageUrl: product.images?.[0]?.url || null,
+            priceAtacarejo: product.priceAtacarejo,
+            priceAtacado: product.priceAtacado,
+            minAtacarejoQty: product.minAtacarejoQty,
+            minAtacadoQty: product.minAtacadoQty,
+          }}
+          isOpen={open}
+          onClose={() => setOpen(false)}
+        />
+      )}
+    </>
+  );
 }
 
