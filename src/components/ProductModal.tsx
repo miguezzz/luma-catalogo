8b86e46aae6a9da2b471c780c55dfa811145275c
Victor Miguez <v.miguez2004@gmail.com>
2025-05-10 20:39:07 -0300

    fix: fix modal height

--- src/components/ProductModal.tsx
+++ src/components/ProductModal.tsx
@@ -51,7 +51,7 @@ export default function ProductModal({ product, isOpen, onClose }: ProductModalP
 
       {/* modal */}
       <div className="fixed inset-0 flex items-center justify-center z-50 p-4">
-        <div className="bg-white rounded-2xl w-full max-w-md p-6 shadow-[0_0_20px_rgba(255,255,255,1)] relative h-4/5">
+        <div className="bg-white rounded-2xl w-full max-w-md p-6 shadow-[0_0_20px_rgba(255,255,255,1)] relative h-11/12">
           {/* close */}
           <button
             className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
