e4279b1bca2fbe11c61d71cfa75918679f54dadf
Victor Miguez <v.miguez2004@gmail.com>
2025-05-18 19:45:30 -0300

    style: mid (tablet) home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,16 +38,16 @@ export default async function Home() {
       {/* header */}
       <header className="flex flex-col pt-16 items-center justify-center">
         <h1 className="flex justify-center text-6xl font-bold">Catálogo</h1>
-        <p className="flex text-sm justify-center pl-6 xl:pl-0 2xl:pl-0 md:pl-0 pt-3 font-semibold 2xl:text-2xl">Clique na categoria desejada para acessar a página de produtos.</p>
+        <p className="flex text-sm justify-center pl-6 xl:pl-0 2xl:pl-0 md:pl-0 md:mb-2 pt-3 font-semibold md:text-2xl 2xl:text-2xl">Clique na categoria desejada para acessar a página de produtos.</p>
       </header>
 
       {/* category cards */}
-      <div className="grid grid-cols-2 p-4 gap-4 2xl:p-16 2xl:grid-cols-4">
+      <div className="grid grid-cols-2 p-4 gap-4 md:gap-7 2xl:p-16 md:grid-cols-3 2xl:grid-cols-4">
         {categoriesList.map((categoryItem: ICategoryItem) => (
           <Link href={`/${categoryItem.slug}`} key={categoryItem.slug}>
-            <Card className="flex bg-white/20 backdrop-blur-md rounded-xl shadow-xl max-w-md border-none transform hover:scale-101 hover:shadow-[0_0_20px_rgba(255,255,255,1)] transition-all ease-in-out duration-300 h-75 2xl:h-138 overflow-hidden" key={categoryItem.slug}>
+            <Card className="flex bg-white/20 backdrop-blur-md rounded-xl shadow-xl max-w-md border-none transform hover:scale-101 hover:shadow-[0_0_20px_rgba(255,255,255,1)] transition-all ease-in-out duration-300 h-75 md:h-115 2xl:h-138 overflow-hidden" key={categoryItem.slug}>
               <CardHeader>
-                <CardTitle className="text-2xl 2xl:text-3xl 2xl:font-bold">
+                <CardTitle className="text-2xl md:text-3xl 2xl:text-3xl font-bold">
                   {categoryItem.name}
                 </CardTitle>
                 {/* <CardDescription className="text-xm text-gray-900 2xl:mb-2 2xl:text-lg font-semibold">
@@ -60,7 +60,7 @@ export default async function Home() {
                   alt={`Categoria ${categoryItem.name}`}
                   width={400}
                   height={400}
-                  className="flex rounded-2xl object-cover object-center w-full h-50 2xl:h-100 mb-4"
+                  className="flex rounded-2xl object-cover object-center w-full h-50 md:h-88 2xl:h-100 mb-4"
                 />
               </CardContent>
             </Card>
